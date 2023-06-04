2a4deab99197a509b881a443e727159f19553084
arthur andrade <arthurandrad.23@gmail.com>
2023-06-04 20:18:08 -0300

    going to create a component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
 export const Todo = ({task, deleteTodo, toggleComplete}) => {
- 
+  const [lineThrough, setlineThrough] = useState(0);
+
+  function taskLineThrough() {
+    // eslint-disable-next-line no-undef
+    setlineThrough (current => !current)
+  }
+
   return (
     <div className="Todo">
-        <input type='checkbox' className='checkbox'/>
-        <p>{task.task}</p>
+        <input type='checkbox' className='checkbox' onClick={taskLineThrough}/>
+        <label style=
+        {{textDecoration: lineThrough ? "line-through" : "none",
+        color: lineThrough ? "#d8d8d8" : "#000000",}}
+        >
+          {task.task}
+        </label>
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
     </div>
   )
