c4c903a026b619d9c5763eb5da240da002955130
arthur andrade <arthurandrad.23@gmail.com>
2023-06-04 18:14:41 -0300

    holy cow that worked, now look what im doing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+export const Todo = ({task, deleteTodo, toggleComplete}) => {
  
   return (
     <div className="Todo">
-        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
-        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <input type='checkbox' className='checkbox'/>
+        <p>{task.task}</p>
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
-        </div>
     </div>
   )
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
-import { EditTodoForm } from "./EditTodoForm";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -32,23 +31,14 @@ export const TodoWrapper = () => {
     );
   }
 
-  const editTask = (task, id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-      )
-    );
-  };
-
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
-      <TodoForm addTodo={addTodo} />
+      <div className="header">
+        <h1>Get Things Done!</h1>
+        <TodoForm addTodo={addTodo} />
+      </div>
       {/* display todos */}
       {todos.map((todo) =>
-        todo.isEditing ? (
-          <EditTodoForm editTodo={editTask} task={todo} />
-        ) : (
           <Todo
             key={todo.id}
             task={todo}
@@ -56,7 +46,6 @@ export const TodoWrapper = () => {
             editTodo={editTodo}
             toggleComplete={toggleComplete}
           />
-        )
       )}
     </div>
   );
